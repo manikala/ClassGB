105daa7ac7f252198e7bc841e014f535a113f765
Valeriy <manikala777@rambler.ru>
2022-01-20 23:04:11 +0300

    Fix bag

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hi");
-        System.out.println("Write for home")
+        System.out.println("Write for home");
     }
 }
