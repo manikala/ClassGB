571628f584e2034a476ea234e529dafdfc109504
Valeriy <manikala777@rambler.ru>
2022-01-24 11:17:26 +0300

    Калькулятор
    Но нужно обработать все варианты ввода

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hi");
-        System.out.println("Write for home");
+        System.out.println("Введите операция: ");
+        System.out.println("1. Сложение");
+        System.out.println("2. Вычитание");
+        System.out.println("3. Умножение");
+        System.out.println("4. Деление");
+        Scanner scanner = new Scanner(System.in);
+        int oper = scanner.nextInt();
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+        int result;
+
+        if (oper == 1) {
+            result = a + b;
+        }
+        else if (oper == 2) {
+            result = a - b;
+        }
+        else if (oper == 3){
+            result = a * b;
+        }
+        else{
+            result = a / b;
+
+        }
+        System.out.println("Результат: " + result);
+
     }
 }
